f158cd459217137e55c0e6e145c8b43f87833de2
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-21 19:51:01 -0500

    small css changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,8 +4,27 @@
 
 var $dateTime = $(".dateTime");
 let now = dayjs();
+var hour = "h";
+var $h75 = $(".h-75");
+var $empty = $(".empty")
+var $notes = $(".notes")
 
+// h2 date header
 function date() {
     $dateTime.text(now.format("dddd, MMMM D, YYYY h:mm A"));
 };
 date();
+
+function pastPresentFuture() {
+    if (hour === dayjs().hour())
+        $($h75).className(".present")
+    else if (hour > dayjs().hour())
+        $($h75).className(".past")
+    else if (hour < dayjs().hour())
+        $($h75).className(".future")
+};
+pastPresentFuture();
+
+$($empty).on("click", function () {
+    $($notes).empty();
+});
